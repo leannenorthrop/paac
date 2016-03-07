b0b64ac48997c136ca0b1828c56fcafb8009a2e9
Periklis <periklis.symeonidis@digital.hmrc.gov.uk>
2016-03-07 16:55:27 +0000

    Added PensionSpec and CalculatorControllerSpec

--- test/controllers/CalculatorControllerSpec.scala
+++ test/controllers/CalculatorControllerSpec.scala
@@ -1,9 +1,42 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
-/**
-  * Created by peri on 02/03/16.
-  */
-object CalculatorControllerSpec {
+import play.api.libs.json.{JsValue, Json}
+import play.api.test.FakeRequest
+import uk.gov.hmrc.play.test.UnitSpec
+
+import play.api.http.Status
+import play.api.libs.json.{JsValue, Json}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
 
 
+class CalculatorControllerSpec extends UnitSpec {
+
+//  implicit val request = FakeRequest()
+//
+//
+//  "CalculatorController" should {
+//
+//    "return result NOT_FOUND" in {
+//      val result = route(FakeRequest(GET, "/paac/calculate/"))
+//      result.isDefined shouldBe true
+//      status(result.get) should not be NOT_FOUND
+//    }
+//  }
 }
