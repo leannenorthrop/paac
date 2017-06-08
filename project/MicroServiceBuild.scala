3eb2f874bae38ec140fc6c78418d4fa137e68443
Leanne Northrop <leanne.northrop@gmail.com>
2017-06-08 15:57:39 +0100

    DDCNLS-1237: Removing url binder library

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -14,7 +14,6 @@ private object AppDependencies {
   private val microserviceBootstrapVersion = "5.14.0"
   private val playHealthVersion = "2.1.0"
   private val logbackJsonLoggerVersion = "3.1.0"
-  private val playUrlBindersVersion = "2.1.0"
   private val playConfigVersion = "4.3.0"
   private val domainVersion = "4.1.0"
   private val hmrcTestVersion = "2.3.0"
@@ -30,7 +29,6 @@ private object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
     "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
@@ -72,4 +70,3 @@ private object AppDependencies {
 
   def apply() = compile ++ Test() ++ IntegrationTest()
 }
-
